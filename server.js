64844e55333fdcac65ac0eda0b9ec16a6b54d106
Thiago Orlandini <thiagorlandini@hotmail.com>
2018-07-31 03:37:39 -0300

    teste

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req, res) {
-  
-    res.sendFile(__dirname + '/dist/index.html');
-}); 
-
-app.listen(process.env.PORT || 4200);
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/dist/ShawAndPartners/index.html')); //aqui você define onde está o index.html da sua aplicação.
+app.listen(process.env.PORT || 3000);
